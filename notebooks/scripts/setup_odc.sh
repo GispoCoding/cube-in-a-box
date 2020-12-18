46feb3d7e5f29ecbd4e8be292e175cb57d54093a
Mikael Vaaltola <mvaaltola@gmail.com>
2020-12-18 11:33:26 +0200

    Read datacube db url env variable

--- notebooks/scripts/setup_odc.sh
+++ notebooks/scripts/setup_odc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export DATACUBE_DB_URL=${DATACUBE_DB_URL:-'postgresql://mikael@localhost:5413/opendatacube'}
 datacube -v system init
 datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.yaml
 datacube product add ../products/s2_granules.yaml
