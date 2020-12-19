16a58cc67c4c7e625f72525df6b511dc947430d6
Mikael Vaaltola <mvaaltola@gmail.com>
2020-12-19 13:32:10 +0200

    Clean index script, add properties

--- notebooks/index_l1c.py
+++ notebooks/index_l1c.py
@@ -1,24 +1,24 @@
 # GDAL v3.0.4
 import os
 import logging
-from multiprocessing import Process, current_process, Manager, cpu_count
+from multiprocessing import current_process, Manager
 from pathlib import Path
 from queue import Empty
 from typing import Dict
 
 import boto3
 from boto3.session import Session
-import click
 from xml.etree import ElementTree
 from hashlib import md5
 
 import datacube
 from datacube.index.hl import Doc2Dataset
-from datacube.utils import changes, aws
+from datacube.utils import changes
 
 GUARDIAN = "GUARDIAN_QUEUE_EMPTY"
 L1C_BUCKET = "sentinel-s2-l1c"
 L2A_BUCKET = "sentinel-s2-l2a"
+
 
 def _parse_value(s):
     s = s.strip('"')
@@ -40,18 +40,20 @@ def get_s3_url(bucket_name: str, obj_key: str):
 
 
 def absolutify_paths(doc, bucket_name, obj_key):
-    objt_key = format_obj_key(obj_key)
+    obj_key = format_obj_key(obj_key)
     measurements = doc["measurements"]
     for measurement in measurements:
-        measurements[measurement]["path"] = get_s3_url(bucket_name, objt_key + '/' + measurements[measurement]["path"])
+        measurements[measurement]["path"] = get_s3_url(bucket_name, obj_key + '/' + measurements[measurement]["path"])
     return doc
 
+
 def format_s3_key(bucket_name: str, key: str) -> (str, str):
-    keypath = Path(key)
-    keyparts = list(keypath.parts)
-    regioncode = "".join(keyparts[1:4])
-    uri = 's3://' + str(Path(bucket_name / keypath.parent))
-    return uri, regioncode
+    key_path = Path(key)
+    key_parts = list(key_path.parts)
+    region_code = "".join(key_parts[1:4])
+    uri = 's3://' + str(Path(bucket_name / key_path.parent))
+    return uri, region_code
+
 
 def absolutify_s3_paths(doc: Dict, bucket_name: str, key: str) -> Dict:
     measurements = doc["measurements"]
@@ -60,6 +62,7 @@ def absolutify_s3_paths(doc: Dict, bucket_name: str, key: str) -> Dict:
         measurement_key = uri + "/" + measurements[measurement]["path"]
         measurements[measurement]["path"] = measurement_key
     return doc
+
 
 def add_dataset(doc, uri, index: datacube.index.index.Index, **kwargs):
     logging.info("Indexing %s", uri)
@@ -78,126 +81,93 @@ def add_dataset(doc, uri, index: datacube.index.index.Index, **kwargs):
 
     return dataset, err
 
+
 def generate_eo3_dataset_doc(bucket_name: str, key: str, data: ElementTree) -> dict:
     """ Ref: https://datacube-core.readthedocs.io/en/latest/ops/dataset_documents.html """
-    uri, regioncode = format_s3_key(bucket_name, key)
-#     keypath = Path(key)
-#     keyparts = list(keypath.parts)
-#     regioncode = "".join(keyparts[1:4])
-#     uri = 's3://' + str(Path(bucket_name / keypath.parent))
+    uri, region_code = format_s3_key(bucket_name, key)
 
-    sensing_time = data.findall("./*/SENSING_TIME")[0].text
-    crs_code = data.findall("./*/Tile_Geocoding/HORIZONTAL_CS_CODE")[0].text.lower()
-    product_names = {
-        L1C_BUCKET: "s2a_level1c_granule",
-        L2A_BUCKET: "s2a_sen2cor_granule"
-    }
+    tile_id = data.find("./*/TILE_ID").text
+    sensing_time = data.find("./*/SENSING_TIME").text
+    crs_code = data.find("./*/Tile_Geocoding/HORIZONTAL_CS_CODE").text.lower()
+    sun_zenith = float(data.find("./*/Tile_Angles/Mean_Sun_Angle/ZENITH_ANGLE").text)
+    sun_azimuth = float(data.find("./*/Tile_Angles/Mean_Sun_Angle/AZIMUTH_ANGLE").text)
+    grids = read_grid_metadata(data)
 
-    nrows_10 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='10']/NROWS")[0].text)
-    ncols_10 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='10']/NCOLS")[0].text)
-    ulx_10 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='10']/ULX")[0].text)
-    uly_10 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='10']/ULY")[0].text)
-    xdim_10 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='10']/XDIM")[0].text)
-    ydim_10 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='10']/YDIM")[0].text)
-    trans_10 = [xdim_10, 0.0, ulx_10, 0.0, ydim_10, uly_10, 0.0, 0.0, 1.0]
-
-    nrows_20 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='20']/NROWS")[0].text)
-    ncols_20 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='20']/NCOLS")[0].text)
-    ulx_20 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='20']/ULX")[0].text)
-    uly_20 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='20']/ULY")[0].text)
-    xdim_20 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='20']/XDIM")[0].text)
-    ydim_20 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='20']/YDIM")[0].text)
-    trans_20 = [xdim_20, 0.0, ulx_20, 0.0, ydim_20, uly_20, 0.0, 0.0, 1.0]
-
-    nrows_60 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='60']/NROWS")[0].text)
-    ncols_60 = int(data.findall("./*/Tile_Geocoding/Size[@resolution='60']/NCOLS")[0].text)
-    ulx_60 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='60']/ULX")[0].text)
-    uly_60 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='60']/ULY")[0].text)
-    xdim_60 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='60']/XDIM")[0].text)
-    ydim_60 = float(data.findall("./*/Tile_Geocoding/Geoposition[@resolution='60']/YDIM")[0].text)
-    trans_60 = [xdim_60, 0.0, ulx_60, 0.0, ydim_60, uly_60, 0.0, 0.0, 1.0]
-
-    # l1c
-#     ten_list = ['B02_10m', 'B03_10m', 'B04_10m', 'B08_10m']
-#     twenty_list = ['B05_20m', 'B06_20m', 'B07_20m', 'B11_20m', 'B12_20m', 'B8A_20m',
-#                    'B02_20m', 'B03_20m', 'B04_20m', 'B08_20m']
-#     sixty_list = ['B01_60m', 'B02_60m', 'B03_60m', 'B04_60m', 'B8A_60m', 'B09_60m',
-#                   'B05_60m', 'B06_60m', 'B07_60m', 'B11_60m', 'B12_60m', 'B10_60m', 'B08_60m']
-
-    measurement_list = ["B01_60m", "B02_10m", "B03_10m", "B04_10m", "B05_20m",
+    l1c_measurements = ["B01_60m", "B02_10m", "B03_10m", "B04_10m", "B05_20m",
                         "B06_20m", "B07_20m", "B08_10m", "B09_60m", "B8A_20m",
                         "B10_60m", "B11_20m", "B12_20m"]
-
-    # l2a
-    #ten_list = ['B02_10m', 'B03_10m', 'B04_10m', 'B08_10m']
-    #twenty_list = ['B05_20m', 'B06_20m', 'B07_20m', 'B11_20m', 'B12_20m', 'B8A_20m',
-    #               'B02_20m', 'B03_20m', 'B04_20m', B08_20m]
-    #sixty_list = ['B01_60m', 'B02_60m', 'B03_60m', 'B04_60m', 'B8A_60m', 'B09_60m',
-    #              'B05_60m', 'B06_60m', 'B07_60m', 'B11_60m', 'B12_60m']
-
     eo3 = {
         "id": md5(uri.encode("utf-8")).hexdigest(),
         "$schema": "https://schemas.opendatacube.org/dataset",
         "product": {
-            #"name": product_names[bucket_name],
             "name": "s2a_level1c_granule",
         },
         "crs": crs_code,
         "grids": {
-            "default": {
-                "shape": [nrows_10, ncols_10],
-                "transform": trans_10,
+            "default": {  # 10m
+                "shape": [grids["10"]["nrows"], grids["10"]["ncols"]],
+                "transform": grids["10"]["trans"],
             },
             "20m": {
-                "shape": [nrows_20, ncols_20],
-                "transform": trans_20,
+                "shape": [grids["20"]["nrows"], grids["20"]["ncols"]],
+                "transform": grids["20"]["trans"],
             },
             "60m": {
-                "shape": [nrows_60, ncols_60],
-                "transform": trans_60,
+                "shape": [grids["60"]["nrows"], grids["60"]["ncols"]],
+                "transform": grids["60"]["trans"],
             },
         },
         "measurements": {},
         "location": uri,
-        "properties": {  # TODO: add cloud cover from metadata
+        "properties": {
+            "tile_id": tile_id,
             "eo:instrument": "MSI",
             "eo:platform": "SENTINEL-2A",  # TODO: read A or B from metadata
+            "odc:file_format": "JPEG2000",
             "datetime": sensing_time,
-            "odc:file_format": "JPEG2000",  # TODO: check validity
-            "odc:region_code": regioncode,
+            "odc:region_code": region_code,
+            "mean_sun_zenith": sun_zenith,
+            "mean_sun_azimuth": sun_azimuth,
         },
         "lineage": {},
     }
 
-    for measurement in measurement_list:
+    for measurement in l1c_measurements:
         band, resolution = measurement.split("_")
         eo3["measurements"][band] = {"path": f"{band}.jp2"}
-        if resolution == '10m':
-            grid = 'default'
+        if resolution == "10m":
+            grid = "default"
         else:
             grid = resolution
         eo3["measurements"][band]["grid"] = grid
 
-#     for measurement in ten_list:
-#         band, resolution = measurement.split("_")
-#         eo3["measurements"][measurement] = {"path": f"R{resolution}/{band}.jp2"}
-#     for measurement in twenty_list:
-#         band, resolution = measurement.split("_")
-#         eo3["measurements"][measurement] = {
-#             "path": f"R{resolution}/{band}.jp2",
-#             "grid": "20m",
-#         }
-#     for measurement in sixty_list:
-#         band, resolution = measurement.split("_")
-#         eo3["measurements"][measurement] = {
-#             "path": f"R{resolution}/{band}.jp2",
-#             "grid": "60m",
-#         }
-#     eo3["measurements"]["SCL_20m"] = {
-#         "path": "R20m/SCL.jp2",
-#         "grid": "20m",
-#     }
     return absolutify_s3_paths(eo3, bucket_name, key)
+
+
+def read_grid_metadata(data):
+    grids = {
+        "10": {},
+        "20": {},
+        "60": {},
+    }
+    for resolution in grids:
+        grids[resolution]["nrows"] = int(data.findall(
+            f"./*/Tile_Geocoding/Size[@resolution='{resolution}']/NROWS")[0].text)
+        grids[resolution]["ncols"] = int(data.findall(
+            f"./*/Tile_Geocoding/Size[@resolution='{resolution}']/NCOLS")[0].text)
+        grids[resolution]["ulx"] = float(data.findall(
+            f"./*/Tile_Geocoding/Geoposition[@resolution='{resolution}']/ULX")[0].text)
+        grids[resolution]["uly"] = float(data.findall(
+            f"./*/Tile_Geocoding/Geoposition[@resolution='{resolution}']/ULY")[0].text)
+        grids[resolution]["xdim"] = float(data.findall(
+            f"./*/Tile_Geocoding/Geoposition[@resolution='{resolution}']/XDIM")[0].text)
+        grids[resolution]["ydim"] = float(data.findall(
+            f"./*/Tile_Geocoding/Geoposition[@resolution='{resolution}']/YDIM")[0].text)
+        grids[resolution]["trans"] = [grids[resolution]["xdim"], 0.0, grids[resolution]["ulx"], 0.0,
+                                      grids[resolution]["ydim"], grids[resolution]["uly"],
+                                      0.0, 0.0, 1.0]
+    return grids
+
 
 def worker(session: Session, bucket_name: str, queue, config=None):
     dc = datacube.Datacube(config=config)
@@ -215,8 +185,6 @@ def worker(session: Session, bucket_name: str, queue, config=None):
             content = str(raw, "utf-8")
             data = ElementTree.fromstring(content)
             dataset_doc = generate_eo3_dataset_doc(bucket_name, key, data)
-            #uri = get_s3_url(bucket_name, key)
-            #uri = 's3://' + str(Path(bucket_name / Path(key).parent))
             uri = format_s3_key(bucket_name, key)[0]
             add_dataset(dataset_doc, uri, index)
             queue.task_done()
@@ -225,24 +193,30 @@ def worker(session: Session, bucket_name: str, queue, config=None):
         except EOFError:
             break
 
-# prepare q
-manager = Manager()
-queue = manager.Queue()
 
-session = boto3.Session(
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-    region_name='eu-central-1',
-)
-s3 = session.resource('s3')
-bucket = s3.Bucket(L1C_BUCKET)
+def main():
+    # prepare queue
+    manager = Manager()
+    queue = manager.Queue()
 
-for obj in bucket.objects.filter(Prefix='tiles/35/P/PM/2020/10/', RequestPayer='requester'):
-    if obj.key.endswith('metadata.xml'):
-        queue.put(obj.key)
+    session = boto3.Session(
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        region_name='eu-central-1',
+    )
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(L1C_BUCKET)
 
-queue.put(GUARDIAN)
+    # TODO: index other areas
+    for obj in bucket.objects.filter(Prefix='tiles/35/P/PM/2020/10/', RequestPayer='requester'):
+        if obj.key.endswith('metadata.xml'):
+            queue.put(obj.key)
+    q_size = queue.qsize()
+    print(f"indexing {q_size} tiles")
+    queue.put(GUARDIAN)
 
-worker(session, L1C_BUCKET, queue)
+    worker(session, L1C_BUCKET, queue)  # TODO: multithread if necessary
+    print(f"finished indexing {q_size} tiles")
 
-print("finished")
+if __name__ == "__main__":
+    main()
