391f73962e0f96a0577acf5172b0023b0e796d9c
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-07 13:40:59 +0200

    Add s2cloudless odc product

--- notebooks/scripts/setup_odc.sh
+++ notebooks/scripts/setup_odc.sh
@@ -4,3 +4,4 @@ export DATACUBE_DB_URL=${DATACUBE_DB_URL:-'postgresql://mikael@localhost:5413/op
 datacube -v system init
 datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.yaml
 datacube product add ../products/s2_granules.yaml
+datacube product add ../products/s2cloudless_masks.yaml
