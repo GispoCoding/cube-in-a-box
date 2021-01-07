351823cd4645d9a4e636e7ffc166f4620d7db324
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-07 12:53:18 +0200

    Update clear script to use DATACUBE_DB_URL

--- notebooks/scripts/clear_s2_products.sh
+++ notebooks/scripts/clear_s2_products.sh
@@ -1,6 +1,5 @@
 # make sure $PGPASSWORD env variable is set
-psql -U opendatacube -d opendatacube -h localhost -v  product_name=s2a_level1c_granule -f delete_odc_product.sql
-psql -U opendatacube -d opendatacube -h localhost -v  product_name=s2b_level1c_granule -f delete_odc_product.sql
-psql -U opendatacube -d opendatacube -h localhost -v  product_name=s2a_sen2cor_granule -f delete_odc_product.sql
-psql -U opendatacube -d opendatacube -h localhost -v  product_name=s2_l2a -f delete_odc_product.sql
-
+psql $DATACUBE_DB_URL -v product_name=s2a_level1c_granule -f delete_odc_product.sql
+psql $DATACUBE_DB_URL -v product_name=s2b_level1c_granule -f delete_odc_product.sql
+psql $DATACUBE_DB_URL -v product_name=s2a_sen2cor_granule -f delete_odc_product.sql
+psql $DATACUBE_DB_URL -v product_name=s2_l2a -f delete_odc_product.sql
